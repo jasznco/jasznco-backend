572569f953b772fe3560864ee4773b1d8bb78930
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2025-12-13 15:02:27 +0530

    fix

--- src/config/db.js
+++ src/config/db.js
@@ -8,6 +8,7 @@ const connectDB = async () => {
       useUnifiedTopology: true
     });
     console.log('MongoDB connected');
+
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
